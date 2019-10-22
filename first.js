67873a7eb4f1a0af8ee46957025ab41dd7fa236f
Richard Dowdy <richard.dowdy0@gmail.com>
2019-10-21 21:54:46 -0400

    initial commit of pre-made personal site from flash drive

---
+++ first.js
@@ -0,0 +1,8 @@
+
+var intro = function() {
+  var person = prompt("Please enter your name");
+  let intro = document.getElementById('demo');
+  intro.innerHTML = "Hello " + person + "! How are you today?";
+}
+
+//intro();
