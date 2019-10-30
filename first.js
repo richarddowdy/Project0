48f2a147e4bb302ed72b9beed988aa62fdc602fb
Richard Dowdy <richard.dowdy0@gmail.com>
2019-10-30 13:46:16 -0400

    adding a download icon to the contact page

--- first.js
+++ first.js
@@ -1,8 +1,12 @@
 
-var intro = function() {
-  var person = prompt("Please enter your name");
-  let intro = document.getElementById('demo');
-  intro.innerHTML = "Hello " + person + "! How are you today?";
+for(let i = 0; i < 11; i++){
+  console.log(i);
 }
+
+// var intro = function() {
+//   var person = prompt("Please enter your name");
+//   let intro = document.getElementById('demo');
+//   intro.innerHTML = "Hello " + person + "! How are you today?";
+// }
 
 //intro();
